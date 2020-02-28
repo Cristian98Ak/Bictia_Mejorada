d2864a7fe9b4f02705e3d66690214d1d6e0d63ce
Cristian Rodriguez <quiaxs87@gmail.com>
2020-02-28 09:24:15 -0500

    se agrega carpeta pagina Que hacemos

---
+++ Que_hacemos/Js_WhatIs/header.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    if ($("#menu").offset().top > 56) {
+        $("#menu").addClass("bg-white");
+    } else {
+        $("#menu").removeClass("bg-white");
+    }
+  });
+
+
+
+  
